a98ed91d0abd1456f4ad553c614d62f0a74b7a3a
ArtesiFederico <134456580+ArtesiFederico@users.noreply.github.com>
2023-08-22 17:34:49 -0300

    Update Calculadora.java

--- JAVA/Calculadora.java
+++ JAVA/Calculadora.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Calculadora {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		int operacion; 
 		int numero1, numero2;
